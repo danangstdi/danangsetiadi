e470b2193b410ae4418962f1644ec5a3dc8e870c
danangstdi <dananginfinity@gmail.com>
2025-03-22 23:28:52 +0700

    feat: add tools section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,17 @@
 import Header from "@/components/layouts/Header";
 import BottomNav from "@/components/layouts/BottomNav";
 import Hero from "@/components/ui/home/Hero";
-import { CodeSlash, VectorPen, LayoutTextWindowReverse } from "react-bootstrap-icons";
-import ServiceCard from "@/components/partials/card/ServiceCard";
+import Services from "@/components/ui/home/Services";
+import Tools from "@/components/ui/home/Tools";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <main className="">
+      <main className="pb-20">
         <Hero/>
-        <section id="current-project" className="mt-14 lg:mt-32">
-          <div className="text-center">
-            <h1 className="text-3xl poppins-bold text-gray-800">Our Services</h1>
-            <p className="line-height-2 text-sm text-gray-600 lg:text-base">Our <strong>best services</strong> for your finest needs.</p>
-            <div className="grid mx-10 gap-12 my-14 sm:grid-cols-2 lg:mx-24 xl:mx-72 lg:grid-cols-3">
-              <ServiceCard label='Web Development' icon={<CodeSlash size={32} className=""/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
-              <ServiceCard label='Design Graphic' icon={<VectorPen size={32} className=""/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
-              <ServiceCard label='UI/UX Design' icon={<LayoutTextWindowReverse size={32} className=""/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
-            </div>
-          </div>
-        </section>
+        <Tools/>
+        <Services/>
       </main>
       <BottomNav/>
     </>
---
+++ src/components/ui/home/Tools.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import figma from '@/../public/img/techstack/figma.png'
+import flutter from '@/../public/img/techstack/flutter.png'
+import laravel from '@/../public/img/techstack/laravel.png'
+import mysql from '@/../public/img/techstack/mysql.png'
+import nextjs from '@/../public/img/techstack/nextjs.png'
+import php from '@/../public/img/techstack/php.png'
+import postgresql from '@/../public/img/techstack/postgresql.png'
+import prisma from '@/../public/img/techstack/prisma.png'
+import reactjs from '@/../public/img/techstack/reactjs.png'
+import vue from '@/../public/img/techstack/vue.png'
+
+export default function Tools() {
+  return (
+    <section id="tools" className="lg:mt-36">
+      <marquee behavior="" direction="" className="p-3 bg-gray-100 lg:p-4">
+        <div className="flex items-center gap-8 md:gap-12 lg:gap-20">
+          <Image src={reactjs} alt="" className="h-8 lg:h-10 w-fit" />
+          <Image src={nextjs} alt="" className="h-4 lg:h-6 w-fit" />
+          <Image src={vue} alt="" className="h-6 lg:h-8 w-fit" />
+          <Image src={prisma} alt="" className="h-6 lg:h-8 w-fit" />
+          <Image src={php} alt="" className="h-6 lg:h-8 w-fit" />
+          <Image src={laravel} alt="" className="h-10 lg:h-12 w-fit" />
+          <Image src={flutter} alt="" className="h-8 lg:h-10 w-fit" />
+          <Image src={mysql} alt="" className="h-9 lg:h-11 w-fit" />
+          <Image src={postgresql} alt="" className="h-4 lg:h-6 w-fit" />
+          <Image src={figma} alt="" className="h-10 lg:h-12 w-fit" />
+        </div>
+      </marquee>
+    </section>
+  );
+}
