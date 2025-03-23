c09e913444a0a47d518f15978e9046d07acebd59
danangstdi <dananginfinity@gmail.com>
2025-03-23 11:01:30 +0700

    style: make different color for main service

--- src/components/partials/card/ServiceCard.jsx
+++ src/components/partials/card/ServiceCard.jsx
@@ -1,12 +1,12 @@
 export default function ServiceCard(props) {
   return (
     <div className="card h-80 overflow-hidden rounded-3xl cursor-default">
-       <div className="card2 bg-gradient-to-br from-zinc-700 to-zinc-900 h-full p-3 flex flex-col justify-center items-center gap-3 hover:rounded-3xl">
+       <div className={`card2 bg-gradient-to-br ${props.main ? 'from-emerald-400 to-emerald-600' : 'from-zinc-700 to-zinc-900'} h-full p-3 flex flex-col justify-center items-center gap-3 hover:rounded-3xl`}>
          <div className="bg-white rounded-full p-3 w-fit">
            {props.icon}
          </div>
          <h3 className="text-xl text-white mt-1">{props.label}</h3>
-        <p className="text-sm text-gray-300">{props.text}</p>
+        <p className="text-sm text-gray-200">{props.text}</p>
        </div>
      </div>
   )
--- src/components/ui/home/Services.jsx
+++ src/components/ui/home/Services.jsx
@@ -8,9 +8,9 @@ export default function Services() {
         <h1 className="text-3xl poppins-bold text-gray-800">Our Services</h1>
         <p className="line-height-2 text-sm text-gray-600 lg:text-base">Our <strong>best services</strong> for your finest needs.</p>
         <div className="grid mx-10 gap-12 my-14 sm:grid-cols-2 lg:mx-24 xl:mx-72 lg:grid-cols-3">
-          <ServiceCard label='Web Development' icon={<CodeSlash size={32} className=""/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
-          <ServiceCard label='Design Graphic' icon={<VectorPen size={32} className=""/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
-          <ServiceCard label='UI/UX Design' icon={<LayoutTextWindowReverse size={32} className=""/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
+          <ServiceCard main={true} label='Web Development' icon={<CodeSlash size={32}/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
+          <ServiceCard main={false} label='Design Graphic' icon={<VectorPen size={32}/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
+          <ServiceCard main={false} label='UI/UX Design' icon={<LayoutTextWindowReverse size={32}/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
         </div>
       </div>
     </section>
