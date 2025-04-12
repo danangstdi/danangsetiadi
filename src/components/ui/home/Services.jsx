1ec7adade07a5d379dd39d3fc1550ef75c797b99
danangstdi <dananginfinity@gmail.com>
2025-04-12 10:14:04 +0700

    reduce service gap and card heigh

--- src/components/ui/home/Services.jsx
+++ src/components/ui/home/Services.jsx
@@ -7,7 +7,7 @@ export default function Services() {
       <div className="text-center">
         <h1 className="text-3xl poppins-bold text-gray-800 dark:text-slate-100">Our Services</h1>
         <p className="line-height-2 text-sm text-gray-600 lg:text-base dark:text-slate-300">Our <strong>best services</strong> for your finest needs.</p>
-        <div className="grid mx-10 gap-12 my-14 sm:grid-cols-2 lg:mx-24 xl:mx-72 lg:grid-cols-3">
+        <div className="grid mx-10 gap-8 my-14 sm:grid-cols-2 md:gap-12 lg:mx-24 xl:mx-72 lg:grid-cols-3">
           <ServiceCard main={true} label='Web Development' icon={<CodeSlash size={32}/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
           <ServiceCard main={false} label='Design Graphic' icon={<VectorPen size={32}/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
           <ServiceCard main={false} label='UI/UX Design' icon={<LayoutTextWindowReverse size={32}/>} text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, consequuntur saepe dolorem esse blanditiis iste!'/>
