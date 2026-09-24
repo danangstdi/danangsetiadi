230401d27de4c26ef775d647f8ba507ab5eefb3f
danangstdi <dananginfinity@gmail.com>
2026-09-24 11:33:51 +0700

    separate contact gmail by device

--- src/app/(Home)/_partials/Hero.jsx
+++ src/app/(Home)/_partials/Hero.jsx
@@ -6,6 +6,19 @@ import Avatar from '@/../../public/img/avatar.png'
 import Typewriter from 'typewriter-effect'; 
 
 export default function Hero() {
+  const handleEmail = () => {
+    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      window.location.href = "mailto:danangsetiadi2003@gmail.com";
+    } else {
+      window.open(
+        "https://mail.google.com/mail/?view=cm&fs=1&to=danangsetiadi2003@gmail.com",
+        "_blank"
+      );
+    }
+  };
+
   return (
     <section id="hero" className="mt-24 grid lg:grid-cols-2">
        <div className="flex flex-col items-start gap-8 mt-10 mx-4 lg:mt-32 lg:ml-20 xl:ml-48 lg:mr-0">
@@ -31,10 +44,10 @@ export default function Hero() {
          <p className="line-height-2 text-sm text-gray-600 lg:text-base dark:text-slate-300">
             We offer specialized <strong>website development</strong> services for web developers. With various categories, we also provide complete <strong>resources to develop your web projects.</strong>
         </p>
-         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=danangsetiadi2003@gmail.com" target="_blank" className="text-xs flex items-center gap-2 py-3 px-8 bg-gradient-to-r from-cyan-400 to-pink-500 rounded-full text-white shadow-lg group lg:text-sm">
+         <button onClick={handleEmail} className="text-xs flex items-center gap-2 py-3 px-8 bg-gradient-to-r from-cyan-400 to-pink-500 rounded-full text-white shadow-lg group lg:text-sm">
            <RocketTakeoff size={18} className="group-hover:rotate-45 group-hover:mr-3 group-hover:duration-300 group-hover:ease-in-out"/>
            Contact Me
-         </a>
+         </button>
        </div>
       <figure className="flex justify-center mt-10 h-3/4 sm:h-[85%] lg:h-[145%] lg:mt-0 lg:justify-start lg:ml-28">
         <Image src={Avatar} alt="" className="h-full w-fit z-10 drop-shadow-2xl"/>
